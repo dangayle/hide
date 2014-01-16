42ef8ec5d86965806ea5859956c00f76e9a523b7
Dan Gayle <dang@spokesman.com>
2014-01-16 13:44:34 -0800

    updated unittest

--- tests/test_hide.py
+++ tests/test_hide.py
@@ -2,6 +2,8 @@ import unittest
 import warnings
 import tempfile
 import os
+import shutil
+import zipfile
 import hide.hide as hide
 
 # Usage:
@@ -22,13 +24,24 @@ class test_hide(unittest.TestCase):
         self.old_getpass = hide.getpass
         hide.getpass = fake_getpass
         self.salt = hide.gensalt()
+        tempfile.tempdir = None
+        tempfile.tempdir = tempfile.mkdtemp()
+
         with tempfile.NamedTemporaryFile(delete=False) as f:
             self.temp_file = f
             f.write("bojangles")
 
+        with tempfile.NamedTemporaryFile(delete=False) as z:
+            self.temp_zip = z
+            temp_zip = zipfile.ZipFile(z.name, 'w')
+            temp_zip.writestr("bogart.txt", "bogart")
+            temp_zip.close()
+
     def tearDown(self):
         cleanup_getpass(hide, self.old_getpass)
-        os.unlink(self.temp_file.name)
+        # os.unlink(self.temp_file.name)
+        # os.unlink(self.temp_zip.name)
+        # shutil.rmtree(tempfile.gettempdir())
 
     def test_key_32(self):
         salt = self.salt
@@ -36,11 +49,11 @@ class test_hide(unittest.TestCase):
         self.assertEqual(len(key), 32)
 
     def test_hide(self):
-        # hide.hide(self.temp_file, )
-        # with open(self.temp_file.name) as f:
-        #     content = f.read()
-        #     print content
-        pass
+
+        # pass
+        hide.hide(self.temp_file.name, self.temp_zip.name)
+
+        # print tempfile.gettempdir()
 
     def test_unhide(self):
         # print self.temp_file.name
